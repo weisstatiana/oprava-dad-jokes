711e49e937bd23eb9408243f5012e9bc418412f9
Tatiana Ondzikova <tatianaondzikova4@gmail.com>
2021-06-03 21:10:41 +0200

    nepopdarene hladanie chyby

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -3,19 +3,18 @@ import { render } from 'react-dom';
 import { Joke } from './Joke/Joke';
 import { jokes } from './jokes';
 import './style.css';
-
-const App = () => ( {
-  jokes.map ((joke)=> (
-    <Joke
-    key = {joke.id} 
-    userAvatar={joke.avatar}
-    userName={joke.name}
-    likes ={joke.likes}
-    dislikes={joke.dislikes}
-    />
-  ))
-}
-  );
-
+const App = () => (
+  <div className="container">
+    {jokes.map((joke) => (
+      <Joke
+        key={joke.id}
+        userAvatar={joke.avatar}
+        userName={joke.name}
+        likes={joke.likes}
+        dislikes={joke.dislikes}
+      />
+    ))}
+  </div>
+);
 
 render(<App />, document.querySelector('#app'));
